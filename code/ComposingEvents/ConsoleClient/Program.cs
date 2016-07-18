cd69787da24988ad4542f0555fd64f40be70e3f1
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2016-07-18 11:17:43 +0530

    Initial commit.

---
+++ code/ComposingEvents/ConsoleClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using EventSource;
+using System;
+using System.Reactive.Linq;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var even = new EvenNumberGenerator(10))
+            {
+                using (var odd = new OddNumberGenerator(10))
+                {
+                    var evenObservable = Observable.FromEvent<int>( 
+                        a => even.NumberGenerated += a, 
+                        r => even.NumberGenerated -= r);
+
+                    var oddObservable = Observable.FromEvent<int>
+                        (a => odd.NumberGenerated += a,
+                        r => odd.NumberGenerated -= r);
+
+                    // var composition = evenObservable.Zip(oddObservable, (n1, n2) => n1 * n2);
+
+                    // composition.Subscribe(Console.WriteLine);
+
+                    var composition2 = from n1 in evenObservable
+                                       from n2 in oddObservable
+                                       select n1 * n2;
+
+                    composition2.Subscribe(Console.WriteLine);
+
+                    Console.ReadKey();
+                }
+            }
+        }
+        
+    }
+}
---
+++ code/RetryingOperation/RetryingOperation/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Reactive.Linq;
+
+namespace RetryingOperation
+{
+    class Program
+    {
+        private static bool flag = false;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                string url = "http://www.nonexistent.com";
+
+                var observable = Observable.Return<Func<string, string>>(DownloadString)
+                    .Select(func => func(url));
+
+                observable.Retry(3).Subscribe(Console.WriteLine);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            try
+            {
+
+                string url = "http://www.google.com";
+
+                var observable = Observable.Return<Func<string, string>>(DownloadStringEx)
+                    .Select(func => func(url));
+
+                observable.Retry(3).Subscribe(Console.WriteLine);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.ReadKey();
+        }
+
+        static string DownloadString(string url)
+        {
+            try
+            {
+                return new WebClient().DownloadString(url);
+            }
+            catch
+            {
+                Console.WriteLine("An exception occurred.");
+                throw;
+            }
+        }
+
+        static string DownloadStringEx(string url)
+        {
+            try
+            {
+                if (!flag) url = "http://invalidUrl";
+
+                return new WebClient().DownloadString(url).Length.ToString();
+            }
+            catch
+            {
+                flag = !flag;
+                Console.WriteLine("Ooops!");
+                throw;
+            }
+        }
+    }
+}
