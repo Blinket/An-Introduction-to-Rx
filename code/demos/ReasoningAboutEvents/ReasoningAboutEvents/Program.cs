51089e470a1ab587c7342d9e6f7f88b144548aae
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2016-07-24 11:33:01 +0530

    Added a composite example named OldComposingEvents that demos hot, cold, creating custom observables and using observables as properties.

---
+++ code/demos/ReasoningAboutEvents/ReasoningAboutEvents/Program.cs
@@ -0,0 +1,59 @@
+﻿using SchoolManagementSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace ReasoningAboutEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObserveAdmissionsPeriodically();
+        }
+
+        static void ObserveAdmissionsPeriodically()
+        {
+            var school = new School("School 1");
+
+            var admissionObservable =
+                Observable.FromEventPattern<StudentAdmittedEventArgs>(school, "StudentAdmitted");
+
+            var observables = admissionObservable.Buffer(TimeSpan.FromSeconds(5))
+                .Select(lst => lst.Select(ep => ep.EventArgs));
+
+            var subscription = observables.Subscribe(StudentAdmittedValueHandler);
+
+            school.FillWithStudents(10);
+            school.FillWithStudentsAsync(3, TimeSpan.FromSeconds(10));
+            school.FillWithStudentsAsync(2, TimeSpan.FromSeconds(5));
+
+            Console.WriteLine("Press any key to exit the program");
+            Console.ReadKey();
+            subscription.Dispose();
+        }
+
+        static void StudentAdmittedValueHandler(IEnumerable<StudentAdmittedEventArgs> args)
+        {
+            if (args != null)
+            {
+                var list = args.ToList();
+
+                var count = list.Count;
+
+                if (count > 0)
+                {
+                    Console.WriteLine($"New batch of {count} students received:");
+
+                    list.ForEach(arg =>
+                    {
+                        Console.WriteLine($"{arg.Student} joined {arg.School.Name}");
+                    });
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ code/demos/RetryingOperation/RetryingOperation/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace RetryingOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RetryDownloadString();
+
+            Console.WriteLine("Press any key to exit the program");
+            Console.ReadKey();
+        }
+
+        static void RetryDownloadString()
+        {
+            try
+            {
+                Console.WriteLine($"Main thread: {Thread.CurrentThread.ManagedThreadId}");
+
+                string url = "http://www.nonexistent.com";
+
+                var observable = Observable.Return<Func<string, string>>(DownloadString)
+                    .Select(func => func(url));
+
+                observable.Retry(3).Subscribe(Console.WriteLine, Error, Completed);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Catch reports: {ex.Message}");
+            }
+        }
+
+        static void Completed()
+        {
+            Console.WriteLine("The operation completed successfully.");
+        }
+
+        static void Error(Exception ex)
+        {
+            Console.WriteLine($"Error handler reports: There was an error: {ex.Message}");
+        }
+
+        static string DownloadString(string url)
+        {
+            try
+            {
+                Console.WriteLine($"DownloadString running on thread: {Thread.CurrentThread.ManagedThreadId}");
+
+                return new WebClient().DownloadString(url);
+            }
+            catch
+            {
+                Console.WriteLine("Inside DownloadString: An exception occurred.");
+                throw;
+            }
+        }
+    }
+}
